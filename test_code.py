e6ae60df5512543ce32bd9cb1568b15e80b5dcb2
Reba <reba_philip.sias21@krea.ac.in>
2023-09-20 20:33:45 +0530

    Updated test_code.py and sample data files

--- test_code.py
+++ test_code.py
@@ -1,3 +1,18 @@
-with open("sample_data1.txt", "r") as file:
-	data = file.read()
-	print(data)
+import pandas as pd
+import csv
+
+
+csvfile = "job_data.csv"
+
+def average_salaries(csvfile, job_titles):
+        df = pd.read_csv(csvfile)
+        updated_df = df[df['job_title'].isin(job_titles)]
+        average_salaries = updated_df.groupby('job_title')['num_of_salaries'].mean()
+        return average_salaries
+
+job_titles = ["Data Architect","Senior Business Analyst","Data Scientist","Machine Learning Engineer"]
+
+average_salaries_result = average_salaries(csvfile,job_titles)
+
+print(average_salaries_result)
+
